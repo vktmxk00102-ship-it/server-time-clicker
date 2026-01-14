d9d336d07356189c5145af35ceec56aa65f83e5e
vktmxk00102-ship-it <vktmxk00102@gmail.com>
2026-01-14 17:47:03 +0900

    fix: remove node_modules from git index and update gitignore

--- node_modules/expo-localization/plugin/build/withExpoLocalization.d.ts
+++
@@ -1,8 +0,0 @@
-import type { ExpoConfig } from 'expo/config';
-type ConfigPluginProps = {
-    supportsRTL?: boolean;
-    forcesRTL?: boolean;
-    allowDynamicLocaleChangesAndroid?: boolean;
-};
-declare function withExpoLocalization(config: ExpoConfig, data?: ConfigPluginProps): ExpoConfig;
-export default withExpoLocalization;
