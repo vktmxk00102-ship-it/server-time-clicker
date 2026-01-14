2320adff68fef542b9b2166d76c020962f713bc2
vktmxk <vktmxk00102@gmail.com>
2026-01-14 17:57:44 +0900

    update App.js

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react' ;
 import { StyleSheet, Text, View, TextInput, TouchableOpacity, Alert, ScrollView } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 
